95fff0f2c13ba9826b442be07a54f43d901f6937
leenavarthini <leenavarthini@gmail.com>
2024-06-16 09:50:25 -0400

    created parent

---
+++ src/app/app-parent/app-parent.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-parent',
+  templateUrl: './app-parent.component.html',
+  styleUrls: ['./app-parent.component.css']
+})
+export class AppParentComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppParentComponent } from './app-parent/app-parent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppParentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CommonModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
